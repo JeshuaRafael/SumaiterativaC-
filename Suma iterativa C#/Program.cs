41e2b5011f495ad1dbb31bf301ad26c676460532
hp omen <hp omen@DESKTOP-I0EO1P5>
2023-10-29 23:56:12 -0600

    Agregar archivos de proyecto.

---
+++ Suma iterativa C#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduzca un número:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+
+        while (i < num)
+        {
+            Console.WriteLine("Número actual: " + i);
+            i++;
+        }
+    }
+}
